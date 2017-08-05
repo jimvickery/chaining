7b037911242f44aeccd2786d70284b532afa0778
Jim Vickery <jimrv8@yahoo.com>
2017-08-05 17:18:22 -0500

    add js files

--- chaining.js
+++ chaining.js
@@ -10,8 +10,7 @@ console.log('in chaining.js');
 // integers array 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 //chaining of integers array using conditions above
-let result = ((integers.sort(function(a, b){ return  b - a})).filter(n => n < 20)).map((x)=>{return(x*1.5)-1});
-console.log(result);
+document.getElementById('resultEl').innerHTML = ((integers.sort(function(a, b){ return  b - a})).filter(n => n < 20)).map((x)=>{return(x*1.5)-1});
 
 
 
